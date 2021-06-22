7e2a8edc823342487724aea32c60040a3bee25c7
Jaywye <jaywyeee@users.noreply.github.com>
2021-06-22 15:51:52 +0700

    Add persistent scroll position

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,27 @@
 "use strict";
 
-document.addEventListener('DOMContentLoaded', () => {
-	const buttons = document.querySelectorAll("button");
-	const tabs = document.querySelectorAll("#pr, #cn, #en");
+const buttons = document.querySelectorAll("button");
+const tabs = document.querySelectorAll("[id]");
+let currentTab = "pr";
+let scrollPosition = { "pr": 0, "cn": 0, "en": 0 };
+
+for (let i = 0; i < buttons.length; ++i) {
+	buttons[i].addEventListener('click', switchTab);
+};
+
+function switchTab() {
+	// Save scroll position of current tab
+	scrollPosition[currentTab] = window.scrollY;
+	// Set new current tab
+	currentTab = this.name;
+	// Scroll to saved position of new current tab
+	window.scrollTo(0, scrollPosition[currentTab])
 
 	for (let i = 0; i < buttons.length; ++i) {
-		buttons[i].addEventListener('click', switchTab);
+		buttons[i].classList.remove("selected");
+		tabs[i].classList.remove("selected");
 	};
 
-	function switchTab() {
-		for (let i = 0; i < buttons.length; ++i) {
-			buttons[i].classList.remove("selected");
-		};
-
-		for (let i = 0; i < tabs.length; ++i) {
-			tabs[i].classList.remove("selected");
-		};
-
-		this.classList.add("selected");
-		document.getElementById(this.name).classList.add("selected");
-	};
-});
+	this.classList.add("selected");
+	document.querySelector("#" + this.name).classList.add("selected");
+};
