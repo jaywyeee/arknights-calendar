c2269d44dc2630b237dd63e26086233a37c4ae1e
Jaywye <jaywyeee@users.noreply.github.com>
2021-07-12 11:08:02 +0700

    Implement permalinks

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
 "use strict";
 
+// General
+// Disable persistent scroll position across refreshes.
+if ("scrollRestoration" in history) { history.scrollRestoration = "manual" };
+
+
+
+// Tabs
 const buttons = document.querySelectorAll("button");
 const tabs = document.querySelectorAll("[id]");
+
 let currentTab = "pr";
 let scrollPosition = { "pr": 0, "cn": 0, "en": 0 };
 
+let initialResolve = false;
+
 for (let i = 0; i < buttons.length; ++i) {
-	buttons[i].addEventListener('click', switchTab);
+	buttons[i].addEventListener("click", switchTab);
 };
 
 function switchTab() {
-	// Save scroll position of current tab
+	// Save scroll position of previous tab.
 	scrollPosition[currentTab] = window.scrollY;
 
 	for (let i = 0; i < buttons.length; ++i) {
@@ -21,9 +31,51 @@ function switchTab() {
 	this.classList.add("selected");
 	document.querySelector("#" + this.name).classList.add("selected");
 
-	// Set new current tab
+	// Set current tab.
 	currentTab = this.name;
 
-	// Scroll to saved position of new current tab
-	window.scrollTo(0, scrollPosition[currentTab])
+	// Scroll to saved position of current tab.
+	window.scrollTo(0, scrollPosition[currentTab]);
+
+	// Edit URL to match tab. Skip once if event parameter exists for initial resolve.
+	if (initialResolve === true || !params.get("event")) {
+		const urlParams = (currentTab === "pr") ? "" : "?cal=" + currentTab;
+		const newUrl = baseUrl + urlParams;
+
+		window.history.replaceState({}, "", newUrl);
+	};
+
+	initialResolve = true;
+};
+
+
+
+// Permalinks
+const baseUrl = window.location.origin + window.location.pathname;
+const params = new URLSearchParams(window.location.search);
+const events = document.querySelectorAll("[class|='filled']");
+
+// Resolve initial parameters if any.
+if (params.get("cal")) {
+	const targetButton = document.querySelector(`button[name="${params.get("cal")}"]`);
+	const targetEvent = document.querySelector(`#${params.get("cal")} .${params.get("event")}`);
+
+	targetButton.click();
+	if (targetEvent) { targetEvent.scrollIntoView() };
+};
+
+// Set URL on middle click.
+for (let i = 0; i < events.length; ++i) {
+	events[i].addEventListener("mouseup", setUrl);
+	// Disable middle click scroll.
+	events[i].addEventListener("mousedown", (e) => { if (e.button === 1) return e.preventDefault() });
+};
+
+function setUrl(e) {
+	if (e.button === 1) {
+		const urlParams = `?cal=${currentTab}&event=${this.classList[1]}`;
+		const newUrl = baseUrl + urlParams;
+
+		window.history.replaceState({}, "", newUrl);
+	};
 };
