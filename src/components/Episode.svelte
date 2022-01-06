a0dd561ced575029712c69cac421465fc3033a28
Jaywye <jaywyeee@users.noreply.github.com>
2022-01-06 15:27:54 +0700

    Add support for 2-digit episode numbers

--- src/components/Episode.svelte
+++ src/components/Episode.svelte
@@ -2,10 +2,15 @@
 	export let div;
 
 	const { episode, styles } = div;
+	let number = episode.toString();
+
+	if (number.length === 1) {
+		number = "0" + number;
+	}
 </script>
 
 <div
 	class="episode"
 	style="--grid-row:{styles.row}; --grid-column:{styles.col}">
-		EP<br>0{episode}
+		EP<br>{number}
 </div>
