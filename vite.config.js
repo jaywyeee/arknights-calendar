2da61728c5bd29d1cf4ed4dd7e193ab1385a16f1
jaywyeee <jaywyeee@users.noreply.github.com>
2022-03-16 16:37:22 +0700

    Update for custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
 
 export default defineConfig({
-	base: "/arknights-calendar/",
+	base: "/calendar/",
 	build: {
 		sourcemap: true
 	},
