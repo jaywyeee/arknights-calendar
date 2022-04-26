f2e126571765ea30c3246c33b61d9eb2f64ee6dc
jaywyeee <jaywyeee@users.noreply.github.com>
2022-04-26 16:37:18 +0700

    Switch deployment to Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte"
 export default defineConfig({
 	base: "/calendar/",
 	build: {
+		outDir: "dist/calendar",
 		sourcemap: true
 	},
 	plugins: [svelte()]
