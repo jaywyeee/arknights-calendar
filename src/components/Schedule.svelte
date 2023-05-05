7fe5e036a97e932d852ae89530dc5b449ce1dc5a
jaywyeee <jaywyeee@users.noreply.github.com>
2023-05-05 14:59:11 +0900

    Update future and TW schedules

--- src/components/Schedule.svelte
+++ src/components/Schedule.svelte
@@ -225,32 +225,32 @@
 
 
 	// Drop first month of future schedule if mostly empty.
-	if (page.id === "future") {
-		const [y, m] = startDate;
-		const firstDiv = eventDivs[y][m][0];
-		const lastDiv = eventDivs[y][m].at(-1);
-		const sundayStart = new Date(y, m + 1, 1).getDay() === 0;
-		const nextMonth = m !== 11 ? eventDivs[y][m + 1] : eventDivs[y + 1][0];
+	// if (page.id === "future") {
+	// 	const [y, m] = startDate;
+	// 	const firstDiv = eventDivs[y][m][0];
+	// 	const lastDiv = eventDivs[y][m].at(-1);
+	// 	const sundayStart = new Date(y, m + 1, 1).getDay() === 0;
+	// 	const nextMonth = m !== 11 ? eventDivs[y][m + 1] : eventDivs[y + 1][0];
 
-		if (firstDiv.styles.row >= 3) {
-			months.shift();
-			lastDiv.styles.row = "1";
+	// 	if (firstDiv.styles.row >= 3) {
+	// 		months.shift();
+	// 		lastDiv.styles.row = "1";
 
-			if (lastDiv.order === "end") {
-				lastDiv.name = true;
-			} else if (!lastDiv.order || (sundayStart && lastDiv.order === "start")) {
-				const endDiv = nextMonth.find(div => {
-					return div.event === lastDiv.event && div.order === "end";
-				});
+	// 		if (lastDiv.order === "end") {
+	// 			lastDiv.name = true;
+	// 		} else if (!lastDiv.order || (sundayStart && lastDiv.order === "start")) {
+	// 			const endDiv = nextMonth.find(div => {
+	// 				return div.event === lastDiv.event && div.order === "end";
+	// 			});
 
-				endDiv.name = true;
-			};
+	// 			endDiv.name = true;
+	// 		};
 
-			if (!sundayStart) {
-				nextMonth.unshift(lastDiv);
-			};
-		};
-	};
+	// 		if (!sundayStart) {
+	// 			nextMonth.unshift(lastDiv);
+	// 		};
+	// 	};
+	// };
 
 	// Temporary fix for Ideal City, Reclamation Algorithm, and EP11/IS#3
 	onMount(() => {
