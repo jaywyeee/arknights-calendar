56ebc53680f80559b79b8fa0dbeeb276115dd1a3
Jaywye <jaywyeee@users.noreply.github.com>
2024-01-15 20:12:03 +0900

    Fix broken filepaths in development mode

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte"
 export default defineConfig({
 	base: "/calendar/",
 	build: {
+		assetsDir: "",
 		outDir: "dist/calendar",
 		sourcemap: true
 	},
